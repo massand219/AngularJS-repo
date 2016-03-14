11e2f055d855f002496071e6782d28332e714617
Mehak Massand <massand219@gmail.com>
2016-03-13 22:33:25 -0400

    done with hw4

--- hw4/SampleNode/src/app/main/main.service.js
+++ hw4/SampleNode/src/app/main/main.service.js
@@ -3,14 +3,25 @@
 angular.module('sampleNode')
 
 .factory('TodoAddResource', function($resource) {
-
+	return $resource('/api/addTodo', null, {
+		addTodo: {
+			method: 'PUT',
+			headers: {
+				contentType: 'application/json',
+				accept: 'application/json'
+			}
+		}
+	});
 })
 
 .factory('TodoListResource', function($resource) {
+	return $resource('/api/getTodos');
 })
 
 .factory('TodoDetailsResource', function($resource) {
+	return $resource('/api/getTodoDetails/:id');
 })
 
 .factory('TodoUpdateResource', function($resource) {
+	return $resource('/api/updateTodo');
 });
